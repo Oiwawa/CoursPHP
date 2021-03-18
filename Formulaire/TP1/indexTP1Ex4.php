4dd1ba93a372476adb733edd80fd0191c2515b1d
oiwawa <monstruousbird@protonmail.com>
2021-03-18 09:29:01 +0100

    Cours 3

--- Formulaire/TP1/indexTP1Ex4.php
+++ Formulaire/TP1/indexTP1Ex4.php
@@ -13,20 +13,20 @@
     <legend>Calcul des taxes</legend>
     <form action="indexTP1Ex4.php" method="post">
         <label> Prix HT <br>
-            <input type="number" step="0.01" name="prix_ht"
+            <input type="number" step="0.1" name="prix_ht"
                    value="<?php echo isset($_POST['prix_ht']) ? htmlspecialchars($_POST['prix_ht'], ENT_QUOTES) : ''; ?>">
         </label>
         <br>
-        <label>Taux de TVA <br>
-            <input type="number" step="0.01" name="tva"
+         <label>Taux de TVA <br>
+            <input type="number" step="0.1" name="tva"
                    value="<?php echo isset($_POST['tva']) ? htmlspecialchars($_POST['tva'], ENT_QUOTES) : ''; ?>">
         </label>
         <br>
         <input type="submit" value="Calculer">
 
         <?php
-        $prix_ht = filter_input(INPUT_POST,'prix_ht', FILTER_SANITIZE_NUMBER_FLOAT);
-        $tva = filter_input(INPUT_POST,'tva', FILTER_SANITIZE_NUMBER_FLOAT);
+        $prix_ht = filter_input(INPUT_POST,'prix_ht', FILTER_VALIDATE_FLOAT);
+        $tva = filter_input(INPUT_POST,'tva', FILTER_VALIDATE_FLOAT);
         $prix_ttc = $prix_ht + ($prix_ht * $tva /100);
         $montantTva = ($prix_ht * $tva) /100 ;
 
